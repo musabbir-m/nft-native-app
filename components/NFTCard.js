cb103cc0f45a081d11aa1721ed53486f1779cbd6
musabbir-m <musabbir313@gmail.com>
2023-07-26 15:02:13 +0600

    added Rect button for placing bid and finished NFTCard

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -44,8 +44,12 @@ const NFTCard = ({data}) => {
               alignItems: "center"
             }}> 
               <EthPrice price={data.price}/>
+              <RectButton
+              minWidth={120}
+              fontSize= {SIZES.font}
+              handlePress= {()=> navigation.navigate("Details", {data})}
+              />
             </View>
-
           </View>
     </View>
   )
