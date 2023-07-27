7098bc74ee2af186f8a1de18003ba3e1819f9ec6
musabbir-m <musabbir313@gmail.com>
2023-07-27 12:02:01 +0600

    added details description and current bids

--- screens/Details.js
+++ screens/Details.js
@@ -34,42 +34,51 @@ const {data}= route.params
  console.log(data, 'hello')
   return (
     <SafeAreaView style={{flex:1}}>
-      <FocusedStatusBar
-      barStyle= 'dark-content'
-      backgroundColor= 'transparent'
-      translucent= {true }
-      />
+        <FocusedStatusBar
+        barStyle= 'dark-content'
+        backgroundColor= 'transparent'
+        translucent= {true }
+        />
 
-      <View style={{
-        width: "100%",
-        position: "absolute",
-        bottom:0,
-        paddingVertical: SIZES.font,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(255,255,255,0.5)',
-        zIndex: 1
-      }}>
-        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark}/>
-      </View>
+        <View style={{
+          width: "100%",
+          position: "absolute",
+          bottom:0,
+          paddingVertical: SIZES.font,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'rgba(255,255,255,0.5)',
+          zIndex: 1
+        }}>
+          <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark}/>
+        </View>
      
-     <FlatList
-     data={data.bid}
-     renderItem={({item})=> <DetailsBid bid={item}/>}
-     keyExtractor={(item)=>item.id}
-     showsVerticalScrollIndicator= {false}
-     contentContainerStyle={{paddingBottom: SIZES.extraLarge*3}}
-      ListHeaderComponent={()=>(
-        <React.Fragment>
-          <DetailsHeader data={data} navigation={navigation}/>
-          <SubInfo/>
+      <FlatList
+        data={data.bid}
+        renderItem={({item})=> <DetailsBid bid={item}/>}
+        keyExtractor={(item)=>item.id}
+        showsVerticalScrollIndicator= {false}
+        contentContainerStyle={{paddingBottom: SIZES.extraLarge*3}}
+          ListHeaderComponent={()=>(
+              <React.Fragment>
+                <DetailsHeader data={data} navigation={navigation}/>
+                <SubInfo/>
 
-          <View style={{padding: SIZES.font}}>
-            <DetailsDesc data={data}/>
-          </View>
-        </React.Fragment>
-      )}
-     />
+                  <View style={{padding: SIZES.font}}>
+                    <DetailsDesc data={data}/>
+                
+                      {/* Current bid */}
+                      {data.bids.length>0 && (
+                        <Text style={{
+                          fontSize: SIZES.font,
+                          fontFamily: FONTS.semiBold,
+                          color: COLORS.primary
+                        }}>
+                          Current Bid
+                        </Text>)}
+                  </View>
+              </React.Fragment>
+      )}/>
 
     </SafeAreaView>
   )
